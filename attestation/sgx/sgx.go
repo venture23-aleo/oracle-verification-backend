1fc47a9b408c247e5dfedcacce162862cfec1bd3
shaibriz <97502054+shaibriz@users.noreply.github.com>
2025-09-19 17:27:46 +0545

    fix: enhance SGX report verification to handle TCB level invalid cases ( ref #17)

--- attestation/sgx/sgx.go
+++ attestation/sgx/sgx.go
@@ -6,11 +6,22 @@ import (
 	"log"
 
 	"github.com/edgelesssys/ego/attestation"
+	"github.com/edgelesssys/ego/attestation/tcbstatus"
 	"github.com/edgelesssys/ego/eclient"
 )
 
 func VerifySgxReport(reportBytes []byte, targetUniqueId string) (*attestation.Report, error) {
 	report, err := eclient.VerifyRemoteReport(reportBytes)
+
+	if err == attestation.ErrTCBLevelInvalid {
+		switch report.TCBStatus {
+		case tcbstatus.ConfigurationNeeded, tcbstatus.ConfigurationAndSWHardeningNeeded:
+			// tolerate these under current policy
+		default:
+			return nil, errors.New("report has invalid TCB level")
+		}
+	}
+
 	if err != nil {
 		return nil, err
 	}
